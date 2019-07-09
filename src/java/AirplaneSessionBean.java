03ca5d97813dd1d9c3e3d6960ec98cfd7010465b
giadaanastasi <40028778+giadaanastasi@users.noreply.github.com>
2019-07-09 11:59:58 +0200

    Update AirplaneSessionBean.java

--- src/java/AirplaneSessionBean.java
+++ src/java/AirplaneSessionBean.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import javax.ejb.Singleton;
 import javax.websocket.Session;
 
@@ -21,18 +22,49 @@ import javax.websocket.Session;
 public class AirplaneSessionBean {
 
     static Map<String, Posto> occupati = new HashMap<String, Posto> ();
+    private Map<String, ReentrantReadWriteLock> writeLockMap = new HashMap<String, ReentrantReadWriteLock> ();
+    
+    private ReentrantReadWriteLock readLock = new ReentrantReadWriteLock();
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
     
     public Map<String, Posto> getBloccati(){
+        readLock.readLock().lock();
+        try{
+            
+        }finally{
+            readLock.readLock().unlock();
+        }
         return occupati;
     }
     
     public void insert(String key, int riga, int colonna, String id_utente){
-        occupati.put(key, new Posto(riga, colonna, id_utente));
+        
+        ReentrantReadWriteLock temp = writeLockMap.get(key);
+        temp.writeLock().lock();
+        writeLockMap.replace(key, temp);
+        try{
+             occupati.put(key, new Posto(riga, colonna, id_utente));
+        }finally{
+            temp = writeLockMap.get(key);
+            temp.writeLock().unlock();
+            writeLockMap.replace(key, temp); 
+        }
+        
+       
     }
     
     public void remove(String key){
-        occupati.remove(key);
+        ReentrantReadWriteLock temp = writeLockMap.get(key);
+        temp.writeLock().lock();
+        writeLockMap.replace(key, temp);
+        try{
+            occupati.remove(key);
+        }finally{
+            temp = writeLockMap.get(key);
+            temp.writeLock().unlock();
+            writeLockMap.replace(key, temp); 
+        }
+        
     }
 }
